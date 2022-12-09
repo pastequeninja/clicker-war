99d4e2727cea0adab16f5e2010e355e4e0891fc2
pastequeninja@PC-Ma▒ly <mai-ly.lehoux@epitech.eu>
2022-12-09 17:16:27 +0100

    :green_heart: add prettier job in CI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,7 @@
-import React from "react"
+import React from "react";
 
 const Home = () => {
-
-    return (
-        <div>Hello !</div>
-    )
-}
+  return <div>Hello !</div>;
+};
 
 export default Home;
